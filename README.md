f79381894b32f96c1e940c1d2a985f43e2f819ba
Lesly Williams <82903572+Lwilliams002@users.noreply.github.com>
2023-11-30 09:18:37 -0500

    "Add empty files for LeetCode categories"

---
+++ README.md
@@ -0,0 +1,29 @@
+# My Personal LeetCode Solutions
+
+This repository contains folders for organizing LeetCode problems. Each folder corresponds to a specific category or topic, making it easy to navigate and locate problems for practice.
+
+## Categories
+
+### LeetCode 75
+- **Array**
+- **String**
+- **Two Pointers**
+- **Sliding Window**
+- **Prefix Sum**
+- **Hash Map_Set**
+- **Stack**
+- **Queue**
+- **Linked List**
+- **Binary Tree - DFS**
+- **Binary Tree - BFS**
+- **Binary Search Tree**
+- **Graphs - DFS**
+- **Graphs - BFS**
+- **Heap_Priority Queue**
+- **Binary Search**
+- **Backtracking**
+- **DP - 1D**
+- **DP - Multidimensional**
+- **Trie**
+- **Intervals**
+- **Monotonic Stack**
---
+++ src/Array/MergeStringsAlternately.java
@@ -0,0 +1,75 @@
+package Array;
+/*
+Problem: 1768. Merge Strings Alternately
+Link: https://leetcode.com/problems/merge-strings-alternately/
+
+Description:
+You are given two strings word1 and word2.
+Merge the strings by adding letters in alternating order, starting with word1.
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Time Complexity: O(max(length of word1, length of word2)) O(n), where n is the length of the longer string.
+Space Complexity: O(max(length of word1, length of word2))
+
+ */
+
+
+public class MergeStringsAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                sb.append(word1.charAt(i));
+            }
+            if (j < word2.length()) {
+                sb.append(word2.charAt(j));
+            }
+            i++;
+            j++;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MergeStringsAlternately merger = new MergeStringsAlternately();
+
+        // Test case
+        String result = merger.mergeAlternately("abc", "123");
+        System.out.println("Merged String: " + result);
+    }
+}
+
+
+/*
+Notes:
+
+Inefficient string concatenation using " + "
+String inefficientConcatenation(String[] words) {
+    String result = "";
+    for (String word : words) {
+        result += word;
+    }
+    return result;
+}
+
+Efficient string concatenation using StringBuilder
+String efficientConcatenation(String[] words) {
+    StringBuilder sb = new StringBuilder();
+    for (String word : words) {
+        sb.append(word);
+    }
+    return sb.toString();
+}
+
+Using StringBuilder for string concatenation in a loop is more efficient than
+directly concatenating strings with the + operator, especially when dealing with large
+strings or a large number of concatenations. This is because strings in Java are
+immutable, meaning each time you concatenate them, a new string object is created.
+This can lead to unnecessary memory allocations and can be inefficient in terms of
+both time and space complexity.
+    - it doesn't create a new object each time you modify it.
+ */
