a4f14badd7a416e3ee5e213a6ed7f6787a5ebc49
Lesly Williams <82903572+Lwilliams002@users.noreply.github.com>
2024-02-21 23:14:57 -0500

    updated Array folder

--- Solution/LeetCode75/Array/CanPlaceFlowers.java
+++ Solution/LeetCode75/Array/CanPlaceFlowers.java
@@ -16,5 +16,28 @@ Space Complexity: O(n)
 
  */
 public class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n){
+        int leftPointer = 0;
+        int currentPointer = flowerbed[0];
+        int count = 0;
+        for(int rightPointer = 1; rightPointer < flowerbed.length; rightPointer++){
+            if(currentPointer == 0 && currentPointer == leftPointer && currentPointer == flowerbed[rightPointer]){
+                count++;
+                leftPointer = 1;
+            }else{
+                leftPointer = currentPointer;
+            }
+            currentPointer = flowerbed[rightPointer];
+        }
+        if(leftPointer == 0 && leftPointer == currentPointer){
+            count++;
+        }
+        return count >= n;
+    }
 
+    public static void main(String[] args) {
+        CanPlaceFlowers planting = new CanPlaceFlowers();
+        int i = 1;
+        System.out.println(planting.canPlaceFlowers(new int[]{1, 0, 0, 0, 1},1));
+    }
 }
