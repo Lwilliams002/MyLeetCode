4c4694577f98af0bec02eed2b04570b785cf6297
Lesly Williams <82903572+Lwilliams002@users.noreply.github.com>
2024-02-21 23:05:27 -0500

    Completed 605

---
+++ Solution/LeetCode75/Array/CanPlaceFlowers.java
@@ -0,0 +1,20 @@
+package LeetCode75.Array;
+
+/*
+Problem: 605. Can Place Flowers
+Link: https://leetcode.com/problems/can-place-flowers/description/?envType=study-plan-v2&envId=leetcode-75
+
+Description:
+flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n,
+return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+ */
+public class CanPlaceFlowers {
+
+}
